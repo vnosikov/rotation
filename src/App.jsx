64fd16bf635b1b2936788dac5fc2b84ef6345aef
Vasily Nosikov <vnosikov@gmail.com>
2024-09-18 22:15:08 +0300

    panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import Rotator from './Rotator'
 import './App.css'
 
-function App() {
-  const [animate, setAnimate] = useState(false)
-  const toggle = () => { setAnimate(v => !v) }
-  useEffect(() => {
-    window.addEventListener('click', toggle)
-    return () => {
-      window.removeEventListener('click', toggle)
-    }
-  }, [])
-
-  const animateClass = animate ? '' : 'stop'
+const App = () => {
+  const [showBg, setShowBg] = useState(true)
+  const [bgOpacity, setBgOpacity] = useState(0.3)
+  const [xScaleFactor, setXScaleFactor] = useState(0.6)
+  const [yScaleFactor, setYScaleFactor] = useState(1)
+  const [time, setTime] = useState(7)
   return (
-    <div className="container">
-      <div className={`overlay blurred ${animateClass}`} />
-      <div className="unblurdiv">
-        <div className={`overlay ${animateClass}`} />
+    <div>
+      <div>
+        <div className="field">
+          <span>Показать задник</span>
+          <input type="checkbox" value={showBg} onChange={() => { setShowBg(v => !v)}}/>
+        </div>
+        <div className="field">
+          <span>Размытость задника</span>
+          <input type="number" min={0} max={1} step={0.1} value={bgOpacity} onChange={(e) => { setBgOpacity(e.target.value)}}/>
+        </div>
+        <div className="field">
+          <span>Сжатие по X</span>
+          <input type="number" step={0.1} value={xScaleFactor} onChange={e => { setXScaleFactor(e.target.value)}}/>
+        </div>
+        <div className="field">
+          <span>Сжатие по Y</span>
+          <input type="number" step={0.1} value={yScaleFactor} onChange={e => { setYScaleFactor(e.target.value)}}/>
+        </div>
+        <div className="field">
+          <span>Время оборота</span>
+          <input type="number" step={1} value={time} onChange={e => { setTime(e.target.value)}} />
+        </div>
       </div>
+      <Rotator />
     </div>
   )
 }
